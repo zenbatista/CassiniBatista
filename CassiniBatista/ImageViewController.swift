803123fbabc8045de3ee884df8f8eb0598d9437c
zenbatista <harald.batista@yahoo.com>
2018-01-07 13:21:17 -0800

    Latest code.

---
+++ CassiniBatista/ImageViewController.swift
@@ -0,0 +1,79 @@
+//
+//  ImageViewController.swift
+//  CassiniBatista
+//
+//  Created by Harald Batista on 1/7/18.
+//  Copyright © 2018 zenbatista. All rights reserved.
+//
+
+import UIKit
+
+class ImageViewController: UIViewController, UIScrollViewDelegate {
+
+    var imageURL: URL? {
+        didSet {
+            image = nil
+
+            if view.window != nil {
+                fetchImage()
+            }
+            
+        }
+    }
+    
+    private var image: UIImage? {
+        get {
+            return imageView.image
+        }
+        set {
+            imageView.image = newValue
+            imageView.sizeToFit()
+            scrollView.contentSize = imageView.frame.size
+        }
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        if imageView.image == nil {
+            fetchImage()
+        }
+        
+    }
+    
+    @IBOutlet weak var scrollView: UIScrollView! {
+        didSet {
+            scrollView.minimumZoomScale = 1/25
+            scrollView.maximumZoomScale = 1.0
+            scrollView.delegate = self
+            scrollView.addSubview(imageView)
+        }
+    }
+    
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return imageView
+    }
+    
+    
+    var imageView = UIImageView()
+    
+    private func fetchImage() {
+        if let url = imageURL {
+            let urlContents = try? Data(contentsOf: url)
+            if let imageData = urlContents {
+                image = UIImage(data: imageData)
+
+            }
+            }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if imageURL == nil {
+            imageURL = DemoURLs.stanford
+        }
+    }
+    
+    
+    
+    
+}
